04b82d9e40ed41b8f2f3816fc3b3221b948c497e
LGustavo992 <luizgustavo.fl92@gmail.com>
2024-11-08 16:04:26 -0300

    Add files via upload

---
+++ pretica1.js
@@ -0,0 +1,26 @@
+//Variaveis
+let carro = "Gol";
+let tanqueCombustivel = 40;
+let tanqueAtual = 15;
+let tipoCombustivel = "Gasolina";
+
+let precoGasolina = 5.50;
+let precoAlcool= 3.50;
+
+let formaPagamento = "Crédito";
+let temCupom = true;
+let descontoApp = 0.05;
+let impostoGasolina = 1.05;
+let impostoAlcool = 1.02;
+
+//Funcionalidades
+
+function abastecer(tipoCombustivel) {
+if (tipoCombustivel === "Gasolina"
+    //Calculo Gasolina
+    valorParaAbastecer / precoGasolina
+)
+
+}
+
+//Testes
